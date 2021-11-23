6aa9d405ee2d569e0be3b618e6c16978ba65731d
Harlen Hobbs <hhobbs@affirma.com>
2021-11-23 08:37:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <div>
+            <ul>
+              <li><a><Link to ="/">Home</Link></a></li>
+            </ul>
+            <ul>
+              <li><a><Link to ="/away">Away</Link></a></li>
+            </ul>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/away">
+            <Away />
+          </Route>
+        </Switch>
+        <p className="bg-red-400" >Hello Tailwind</p>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
+        <div className="m-4">
+        <div className="h-48 flex flex-wrap content-start">
+          <div className="bg-red-500 w-1/3 bg-repeat-round">ABC</div>
+          <div className="bg-yellow-200  w-1/3 text-center origin-center">DEF</div>
+          <div className="bg-blue-600 w-1/3 text-white">GHI</div>
+          <div className="w-1/3">Testing 1</div>
+          <div className="w-1/3">Testing 2</div>
+          <div className="w-1/3">Testing 3</div>
+        </div>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Away.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+
+
+const Away = () => {
+
+
+  return (
+    <div className="away">
+      <p>Away</p>
+    </div>
+
+
+   );
+};
+
+export default Away;
